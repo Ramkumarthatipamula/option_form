e935ccf2f614c29009a2d6bcb8dcec091997fffe
ramkumar <ramkumar.ramu1@gmail.com>
2018-02-14 20:40:00 +0530

    vale

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def file_upload(certificate)
+    if File.exist?(Rails.root.join+"public/uploads/Certificate/#{certificate.student_id}.pdf").present?
+      certificate.update(attachment: (Rails.root.join+"public/uploads/Certificate/#{certificate.student_id}.pdf").open)
+      return true
+    else
+      return false
+    end
+  end
 end
